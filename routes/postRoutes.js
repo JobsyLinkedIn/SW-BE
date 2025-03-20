67040fe80e06f00bb0545d6d8b4ba77b60f8cbf3
ZeyadKhaled-29 <z.badry19@gmail.com>
2025-03-20 17:14:21 +0200

    Add [GetFeed,GetSinglePost]  Routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createPostCtrl } from "../controllers/postsController.js"
+import { createPostCtrl, getFeedCtrl, getSinglePostCtrl } from "../controllers/postsController.js"
 const router = express.Router()
 
 // api/posts
 router.route("/")
+    .get(getFeedCtrl)
     .post(createPostCtrl) //We shoule Use Verify Token MiddleWare First
+
+// api/posts/:id
+router.route("/:id")
+    .get(getSinglePostCtrl)
 
 export default router
