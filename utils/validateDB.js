97b1afe6b21f3a4571ced8f41d69b94e2bd64505
ZeyadKhaled-29 <z.badry19@gmail.com>
2025-03-21 18:02:07 +0200

    Add utils/validateDB.js [validateDocumentsExistence,areValidObjectIds] finction

---
+++ utils/validateDB.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+/**
+ * Validate if a single document or list of documents exist in the database.
+ * @param {mongoose.Model} model - The Mongoose model (e.g., User, Post, Comment).
+ * @param {string[]} docIds - Array of document IDs to validate.
+ * @returns {Promise<boolean>} - Returns true if all IDs exist, otherwise false.
+ */
+async function validateDocumentsExistence(model, docIds) {
+    if (docIds.length === 0) {
+        return false;
+    }
+
+    const existingDocsCount = await model.countDocuments({ _id: { $in: docIds } });
+    return existingDocsCount === docIds.length;
+}
+
+/**
+ * Validate if all provided IDs in an array are valid MongoDB ObjectIds.
+ * @param {string[]} ids - Array of IDs to validate.
+ * @returns {boolean} - Returns true if all IDs are valid ObjectIds, otherwise false.
+ */
+function areValidObjectIds(ids) {
+    if (!Array.isArray(ids) || ids.length === 0) {
+        return false;
+    }
+    return ids.every(id => mongoose.Types.ObjectId.isValid(id));
+}
+
+export { validateDocumentsExistence, areValidObjectIds }
