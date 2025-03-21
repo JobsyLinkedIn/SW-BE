c988fb3c19866c280d82995eaff7bfb7a328bce0
ZeyadKhaled-29 <z.badry19@gmail.com>
2025-03-21 02:09:56 +0200

    Add routes for:add comment/remove comment / delete comment /edit comment /get posts comments/ get posts likes / get post shares

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPostCtrl, getFeedCtrl, getSinglePostCtrl, editPostCtrl, likePostCtrl } from "../controllers/postsController.js"
+import { createPostCtrl, getFeedCtrl, getSinglePostCtrl, editPostCtrl, likePostCtrl, addCommentCtrl, deleteCommentCtrl, editCommentCtrl, getPostCommentsCtrl, getPostLikesCtrl, getPostSharesCtrl } from "../controllers/postsController.js"
 const router = express.Router()
 
 // api/posts
@@ -16,4 +16,17 @@ router.route("/:id")
 router.route("/like/:id")
     .put(likePostCtrl)
 
+// api/posts/comment/:id
+router.route("/comment/:id")
+    .post(addCommentCtrl)
+    .delete(deleteCommentCtrl)
+    .put(editCommentCtrl)
+
+// Route to fetch comments of a specific post with pagination
+router.get("/:id/comments", getPostCommentsCtrl);
+
+// Route to fetch users who make like to  a specific post with pagination
+router.get("/:id/likes", getPostLikesCtrl);
+// Route to fetch users who make share to  a specific post with pagination
+router.get("/:id/shares", getPostSharesCtrl);
 export default router
