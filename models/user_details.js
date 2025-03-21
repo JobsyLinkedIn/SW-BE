090521ee45b1f6c99fd12854489e379a245f1fd1
ZeyadKhaled-29 <z.badry19@gmail.com>
2025-03-22 00:33:15 +0200

    Add saved-posts to  models/user_details.js

--- models/user_details.js
+++ models/user_details.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const userDetailsSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   industry: String,
   location: String,
-  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], 
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   connections: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  skills: [String]
+  skills: [String],
+  savedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }]
 });
 
-const UserDetails = mongoose.model("UserDetails", userDetailsSchema,"UserDetails");
+const UserDetails = mongoose.model("UserDetails", userDetailsSchema, "UserDetails");
 
 export default UserDetails;
