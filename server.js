9626b676ec3395fae1476f972bdf96dddb1e2e5c
ZeyadKhaled-29 <z.badry19@gmail.com>
2025-03-21 18:00:10 +0200

    Editing server.js to replace used Error Handler

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from './db.js';
 import authRoutes from './routes/auth.js'; // Import authentication routes
 import profileRoutes from './routes/profileRoutes.js'; // Import profile routes
 import postRoutes from './routes/postRoutes.js'; // Import post routes
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,10 +22,15 @@ app.use('/api/auth', authRoutes); // Authentication routes
 app.use('/api/user/profile', profileRoutes); // Profile routes
 app.use('/api/posts', postRoutes)
 
+// 🔴 Place this at the end (AFTER routes)
+app.use(errorHandler);
+
+/*
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+*/
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
