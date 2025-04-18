98059430565ab40249eccac18353cab12e5f5fd6
Karim-Mohamed10 <karim.ahmed03@eng-st.cu.edu.eg>
2025-04-18 17:45:34 +0200

    Notification schema

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ router.post('/register', authController.register);
 router.get('/verify-email', authController.verify);
 router.post('/login', authController.login);
 router.get('/resend-confirmation-email', authController.resendConfirmation);
-router.post('/forgot-password', authController.forgot);
+router.post('/forget-password', authController.forgot);
 router.post('/reset-password', authController.reset);
 router.post('/change-password', authController.change);
 router.post('/update-email', authController.updateEmailController);
