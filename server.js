af2a00bbe4d3ccfaff7625aae5ffa0d5289a4cde
Asser-Tamer <asserwagdy22@gmail.com>
2025-04-20 18:44:27 +0200

    added jobroutes in server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import NotificationRoutes from './routes/notification.js';
 import http from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 import companyRoutes from './routes/company.js';
+import jobRoutes from './routes/job.js'; // Import job routes
 
 const app = express();
 const PORT = process.env.PORT || 3000;
