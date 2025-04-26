72153c9d7f08a18b487a0c231166da8980802153
Karim-Mohamed10 <karim.ahmed03@eng-st.cu.edu.eg>
2025-04-26 21:16:42 +0300

    Report Service

--- services/reportServices.js
+++ services/reportServices.js
@@ -1,4 +1,6 @@
 import Report from '../models/report.js';
+import { postModel as Post }  from '../models/post.js';
+import User from '../models/user.js';
 
 export const createReport = async ({ type, targetId, reason, details, reportedBy }) => {
   const report = new Report({ type, targetId, reason, details, reportedBy });
@@ -19,11 +21,27 @@ export const getReportById = async (id) => {
 };
 
 export const updateReportStatus = async (id, status, actionTaken = '') => {
-  const updated = await Report.findByIdAndUpdate(
-    id,
-    { status, actionTaken },
-    { new: true }
-  );
-  if (!updated) throw new Error('Report not found');
-  return updated;
+  const report = await Report.findById(id);
+  if (!report) throw new Error('Report not found');
+
+  if (status === 'approved') {
+    if (report.type === 'post') {
+      await Post.findByIdAndDelete(report.targetId);
+    }
+    if (report.type === 'user') {
+      await User.findByIdAndDelete(report.targetId);
+    }
+    await Report.findByIdAndDelete(id);
+    return { message: 'Report accepted and necessary actions taken.' };
+  } else if (status === 'ignored') {
+    report.status = 'ignored';
+    report.actionTaken = actionTaken || 'No action needed';
+    await Report.findByIdAndDelete(id);
+    await report.save();
+    return report;
+  } else {
+    throw new Error('Invalid status. Status must be approved or ignored.');
+  }
+  
 };
+
